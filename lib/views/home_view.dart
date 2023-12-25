8a5604d574ace67d5d9549028086925650c2de08
Youssef Habib <ykmhabib97@gmail.com>
2023-12-25 18:56:53 +0200

    Created custom grid view & refactored code for the items in the grid in a file called custom_item.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+// Widget imports
+import 'package:adaptive_responsive_app/views/widgets/home_view_body.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFDBDBDB),
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: const Icon(
@@ -13,6 +17,7 @@ class HomeView extends StatelessWidget {
           color: Colors.white,
         ),
       ),
+      body: const HomeViewBody(),
     );
   }
 }
---
+++ lib/views/widgets/custom_sliver_grid.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Widget imports
+import 'custom_item.dart';
+
+class CustomSliverGrid extends StatelessWidget {
+  const CustomSliverGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return const CustomItem();
+      },
+      itemCount: 4,
+    );
+  }
+}
