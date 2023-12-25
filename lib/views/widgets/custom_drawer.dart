99931dc43b2edac88275e6f019ff7c149d0b4102
Youssef Habib <ykmhabib97@gmail.com>
2023-12-25 23:52:43 +0200

    Added a custom drawer widget & FontAwesome Icons

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      backgroundColor: const Color(0xFFDBDBDB),
+      child: Column(
+        children: [
+          const DrawerHeader(
+            child: FaIcon(
+              FontAwesomeIcons.solidHeart,
+              size: 48,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
