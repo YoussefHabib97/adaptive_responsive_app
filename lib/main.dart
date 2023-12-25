40ab7006d3cec1a55088ab13e0258cc0834394a3
Youssef Habib <ykmhabib97@gmail.com>
2023-12-25 18:38:00 +0200

    Created custom app bar

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// View imports
+import 'package:adaptive_responsive_app/views/home_view.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
