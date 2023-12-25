16455cd4ce39d5140c1163966cf4901cf4134014
Youssef Habib <ykmhabib97@gmail.com>
2023-12-25 23:46:06 +0200

    Added a custom drawer, converted home view to stateful widget since we're utlizing scaffold key

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
 // Widget imports
+import 'package:adaptive_responsive_app/views/widgets/custom_drawer.dart';
 import 'package:adaptive_responsive_app/views/widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: const Color(0xFFDBDBDB),
+      drawer: const CustomDrawer(),
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            scaffoldKey.currentState!.openDrawer();
+          },
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
         ),
       ),
       body: const HomeViewBody(),
